1c9943faa00e521608be744fc2e2eb0c47d0b0d5
Marcel Ferreira <marcelrodrigues45@yahoo.com.br>
2025-04-25 13:44:41 -0300

    help msg

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,34 @@ use clap::{Parser, Subcommand};
 mod problems;  // Importa o módulo problems/
 
 #[derive(Parser)]
-#[command(author, version, about)]
+#[command(
+    name = "rosalind",
+    author = "Marcel Ferreira",
+    version = "0.1.0",
+    about = "🔬 Rosalind troubleshooting tool (Bioinformatics)",
+    long_about = r#"
+    Examples:
+      rosalind dna --sequence "AGCTTTTCATT"
+      rosalind rna --sequence "GATGGAACTTGACTACGTAA"
+    "#,
+    arg_required_else_help = true,  // Exibe ajuda se nenhum argumento for passado
+)]
 struct Cli {
     #[command(subcommand)]
-    command: Problem,
+    command: Option<Problem>,
 }
 
 #[derive(Subcommand)]
 enum Problem {
-    /// Resolve o problema 'Counting DNA Nucleotides' (DNA)
+    /// Solve the problem ‘Counting DNA Nucleotides’ (DNA)
+    #[command(arg_required_else_help = true)]
     DNA {
         /// Sequência de DNA de entrada
         #[arg(short, long)]
         dna: String,
     },
+    /// Transcribes DNA into RNA (RNA problem)
+    #[command(arg_required_else_help = true)]
     RNA {
         #[arg(short, long)]
         dna: String,
@@ -28,11 +42,14 @@ fn main() {
     let cli = Cli::parse();
 
     match cli.command {
-        Problem::DNA { dna } => {
+        Some(Problem::DNA { dna }) => {
             problems::dna::solve(&dna);
         },
-        Problem::RNA { dna} => {
+        Some(Problem::RNA { dna}) => {
             problems::rna::solve(&dna);
         },
+        None => {
+
+        }
     }
 }
