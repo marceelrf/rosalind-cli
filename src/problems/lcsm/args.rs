6369d9950d6d8b1e337c04a77ea780a0968cdec1
Marcel Ferreira <marcelrodrigues45@yahoo.com.br>
2025-05-18 13:42:36 -0300

    Update the args and solve for lscm problem

--- src/problems/lcsm/args.rs
+++ src/problems/lcsm/args.rs
@@ -1,1 +1,21 @@
-// lcsm - args.rs
+use clap::{Parser, ArgGroup};
+use std::path::PathBuf;
+
+/// Args to LCSM problem
+#[derive(Debug, Parser)]
+#[command(
+    group(
+        ArgGroup::new("input")
+                .required(true)
+                .args(["readfile"])
+    )
+)]
+pub struct LcsmArgs {
+    /// Input a fasta file.
+    #[arg(short = 'f', long)]
+    pub readfile: Option<PathBuf>,
+
+    /// Output file to write results
+    #[arg(short = 'w', long)]
+    pub writefile: Option<PathBuf>,
+}
--- src/problems/lcsm/solve.rs
+++ src/problems/lcsm/solve.rs
@@ -1,1 +1,27 @@
 // lcsm - solve.rs
+use super::args::LcsmArgs;
+use crate::utils::sequence::read_fasta;
+
+pub fn solve(args: LcsmArgs) {
+
+    // Read the path to fasta file
+    let path = args
+            .readfile
+            .as_ref().expect("Mandatory input file")
+            .to_str()
+            .expect("Error converting path to string");
+
+    let seqs = read_fasta(path);
+
+    if seqs.is_empty() {
+        panic!("No sequences found in the FASTA file.");
+    }
+
+    let min_seq = seqs
+        .values()
+        .min_by_key(|seq| seq.len())
+        .expect("Failed to find the shortest sequence");
+
+
+
+}
